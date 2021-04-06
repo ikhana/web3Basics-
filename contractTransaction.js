ccc44b661cfe7b560c431c9d37aa4f51a0a5bc96
unknown <inaamullah484@gmail.com>
2021-04-06 20:28:53 +0500

    nounce to  nonce

--- contractTransaction.js
+++ contractTransaction.js
@@ -9,31 +9,31 @@ var ethjs = require("ethereumjs-tx");
 const rpcUrl = "HTTP://127.0.0.1:8545";
 const  web3 = new Web3(rpcUrl);
 //console.log("rpc", web);
-let address = "0x59b255fB79b6C9e9e62Fdcd3175100727526f2BC";
+let address = "0xA35F68ACD452627a1d46f4a6E00d63a26B3C68d4";
 web3.eth.getBalance(address, (err, wei) => {
     console.log(wei, 'wie')
     balance = web3.utils.fromWei(wei, 'ether')
     console.log("Balance in ether", balance);
    
   })
-let address1 = "0x59b255fB79b6C9e9e62Fdcd3175100727526f2BC";
-const privateKey1 = "6bcef8bd3b7982f11b0cb04367b186ff3d638e2aea7cbddd34f9278b831796cd";
+let address1 = "0xA35F68ACD452627a1d46f4a6E00d63a26B3C68d4";
+const privateKey1 = "42524b96b2bd3e1795f6f81507e8f8bbbd421624872a806dd541423d73b95b0c";
 
-let address2 = "0x404Fe81F3329323bD1665a04145870c7912534Bc";
-const privateKey2 = "b9cfd8c9037c33d4cc099d38d257b4401cf46797b0d39803c80e2c62c31b76ed";
+let address2 = "0xe4E0cC9bbE6BD670616b0fBF28F19322A82F1e78";
+//const privateKey2 = "b9cfd8c9037c33d4cc099d38d257b4401cf46797b0d39803c80e2c62c31b76ed";
 
 const privateKeyBuffer1 = Buffer.from(privateKey1,'hex'); // converts hex to binary 
-const privateKeyBuffer2 = Buffer.from(privateKey2,'hex');
+//const privateKeyBuffer2 = Buffer.from(privateKey2,'hex');
 web3.eth.getTransactionCount(address1, (err, txCount) => {
     let convertToHex = web3.utils.toHex;
     if(err){console.log('error',err)}
     else{
     const txObject ={
-        nounce: convertToHex(txCount++),
+        nonce: convertToHex(txCount),
         to: address2,
-        value: convertToHex(web3.utils.toWei("1", "ether")),
-        gaslimit: convertToHex(6721975),
-        gasPrice: convertToHex(web3.utils.toWei("20000000000","gwei"))
+        value: convertToHex(web3.utils.toWei("6", "ether")),
+        gaslimit: convertToHex(2100000),
+        gasPrice: convertToHex(web3.utils.toWei("10","gwei"))
     
     }
 
@@ -45,6 +45,7 @@ web3.eth.getTransactionCount(address1, (err, txCount) => {
     
     web3.eth.sendSignedTransaction(raw, (err, txHash)=>{
         console.log('Transaction hash is ', txHash);
+        console.log(err)
     })}
 
     
